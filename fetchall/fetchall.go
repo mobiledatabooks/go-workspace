a4e5bbe75af17bffaaa1e97537c546adeb76caa8
Constantine Vassil <smartgolang@gmail.com>
2022-08-21 10:47:29 -0700

    go-workspace_v0.0.1_go-workspace_2022-08-21-10H-47M-27S

---
+++ fetchall/fetchall.go
@@ -0,0 +1,36 @@
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/mobiledatabooks/go-fetch/fetcher"
+)
+
+//!+
+
+func main() { // Fetch prints the content found at each specified URL.
+	fmt.Println("fetcher.FetchWithBuffer: Fetching URLs...") // print message to stdout
+	start := time.Now()                                      // start a timer to measure the time it takes to fetch the URLs
+	for _, url := range os.Args[1:] {                        // for each URL in the command line arguments
+		fetcher.FetchWithBuffer(url) // fetch the URL and print the content
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds()) // print the time elapsed since the start of the timer
+
+	fmt.Println()
+
+	fmt.Println("fetcher.FetchConcurrent: Fetching URLs...") // print message to stdout
+	start = time.Now()                                       // start a timer to measure the time it takes to fetch the URLs
+	ch := make(chan string)                                  // make a channel to receive the results of the fetching of the URLs in parallel (concurrent) and return the results to the channel
+	for _, url := range os.Args[1:] {                        // for each URL in the command line arguments (concurrent)
+		go fetcher.FetchConcurrent(url, ch) // start a goroutine to fetch the URL and return the result to the channel
+	}
+	for range os.Args[1:] { // for each URL in the command line arguments (concurrent) (wait for the results of the goroutines)
+		fmt.Println(<-ch) // receive from channel ch and print the result
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds()) // print the time elapsed since the start of the timer
+}
+
+//!-
---
+++ fetchall/go.mod
@@ -0,0 +1,5 @@
+module mobiledatabooks.com/fetchall
+
+go 1.19
+
+require github.com/mobiledatabooks/go-fetch/fetcher v0.0.0-20220821070707-94a01a7de290
